f86c085061ee72442e71b03331aee23678d62327
ChetryLal <143800158+ChetryLal@users.noreply.github.com>
2023-09-13 13:53:35 +0000

    install

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HealthComponent } from './health/health.component';
 import { GpsComponent } from './gps/gps.component';
 import { MyPetComponent } from './my-pet/my-pet.component';
 import { AlertsComponent } from './alerts/alerts.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'health' },
   { path: 'alerts', component: AlertsComponent },
   { path: 'health', component: HealthComponent },
   { path: 'gps', component: GpsComponent },
-  { path: 'my-pet', component: MyPetComponent }
+  { path: 'my-pet', component: MyPetComponent },
+  { path: 'products', component: ProductsComponent }
 ];
 
 @NgModule({
